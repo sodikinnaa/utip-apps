afc4b22dc1450e96bc7b4e497a3eaee40b64c6e9
sodikinnaa <sodikinnaa@gmail.com>
2025-12-31 17:02:47 +0700

    Refactor TipSlide Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:utip/widgets/person_counter.dart';
+import 'package:utip/widgets/tip_slider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -133,18 +134,7 @@ class _UTipState extends State<UTip> {
 
 
                     // tip slider
-                    Slider(
-                      value: _tipPercentage,
-                      onChanged: (value) {
-                        setState(() {
-                          _tipPercentage = value;
-                        });
-                      },
-                      min: 0,
-                      max: 0.5,
-                      divisions: 5,
-                      label: "${(_tipPercentage*100).round()}%",
-                    ),
+                    TipSlider(tipPercentage: _tipPercentage, onChanged: (double value) { setState(() { _tipPercentage = value; }); },),
                   ],
                 ),
             ),
