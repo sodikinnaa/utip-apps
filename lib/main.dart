f9ac156a53c422e8e9ea681c8cba2b09c88d32c5
sodikinnaa <sodikinnaa@gmail.com>
2026-01-02 09:07:59 +0700

    eroor

--- lib/main.dart
+++ lib/main.dart
@@ -75,27 +75,7 @@ class _UTipState extends State<UTip> {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              padding: const EdgeInsets.all(18),
-              decoration: BoxDecoration(
-                color: theme.colorScheme.inversePrimary,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Column(
-                children: [
-                  Text("Total per Person",
-                    style: style,
-                  ),
-                  Text("$total", style: style.copyWith(
-                    color: theme.colorScheme.onPrimary,
-                    fontSize: theme.textTheme.displaySmall!.fontSize,
-                  )),
-                ],
-              )
-            ),
-          ),
+          TotalPerPerson(theme: theme, style: style, total: total),
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: Container(
@@ -153,6 +133,44 @@ class _UTipState extends State<UTip> {
             ),
           )
         ],
+      ),
+    );
+  }
+}
+
+class TotalPerPerson extends StatelessWidget {
+  const TotalPerPerson({
+    super.key,
+    required this.theme,
+    required this.style,
+    required this.total,
+  });
+
+  final ThemeData theme;
+  final TextStyle style;
+  final double total;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        padding: const EdgeInsets.all(18),
+        decoration: BoxDecoration(
+          color: theme.colorScheme.inversePrimary,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          children: [
+            Text("Total per Person",
+              style: style,
+            ),
+            Text("$total", style: style.copyWith(
+              color: theme.colorScheme.onPrimary,
+              fontSize: theme.textTheme.displaySmall!.fontSize,
+            )),
+          ],
+        )
       ),
     );
   }
